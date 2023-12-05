98c1cecfd13f20ac44a55b5018af6cd1a16f2241
Yashvardhandhondge <152969155+Yashvardhandhondge@users.noreply.github.com>
2023-12-05 16:09:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,25 +6,43 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+// Serve the index.html file
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// Keep track of connected users
+const users = new Set();
+
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log(`User connected: ${socket.id}`);
+  
+  // Add the user to the set of connected users
+  users.add(socket.id);
+  
+  // Emit the updated user list to all clients
+  io.emit('userList', Array.from(users));
 
   // Handle chat messages
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    io.emit('chat message', `${socket.id}: ${msg}`);
   });
 
   // Handle screen sharing
-  socket.on('screen sharing', (data) => {
-    io.emit('screen sharing', data);
+  socket.on('screen sharing', (stream) => {
+    // Broadcast the screen sharing stream to all clients except the sender
+    socket.broadcast.emit('screen sharing', stream);
   });
 
+  // Handle disconnect
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    console.log(`User disconnected: ${socket.id}`);
+    
+    // Remove the user from the set of connected users
+    users.delete(socket.id);
+
+    // Emit the updated user list to all clients
+    io.emit('userList', Array.from(users));
   });
 });
 
